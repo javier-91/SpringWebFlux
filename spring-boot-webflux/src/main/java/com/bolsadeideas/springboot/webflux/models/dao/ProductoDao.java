d4927610e0958f39acd31e9758c8b41942ffd26c
Xavi Sanz <el_pecas91@hotmail.com>
2026-04-07 11:30:40 +0200

    ID + UID

--- spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/models/dao/ProductoDao.java
+++ spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/models/dao/ProductoDao.java
@@ -2,9 +2,8 @@ package com.bolsadeideas.springboot.webflux.models.dao;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-import com.bolsadeideas.springboot.webflux.models.entity.Producto;
+import com.bolsadeideas.springboot.webflux.models.documents.Producto;
 
 public interface ProductoDao extends ReactiveMongoRepository<Producto, String>{
-	
 
 }
--- spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/models/services/ProductoServiceImpl.java
+++ spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/models/services/ProductoServiceImpl.java
@@ -3,8 +3,10 @@ package com.bolsadeideas.springboot.webflux.models.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bolsadeideas.springboot.webflux.models.dao.CategoriaDao;
 import com.bolsadeideas.springboot.webflux.models.dao.ProductoDao;
-import com.bolsadeideas.springboot.webflux.models.entity.Producto;
+import com.bolsadeideas.springboot.webflux.models.documents.Categoria;
+import com.bolsadeideas.springboot.webflux.models.documents.Producto;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -14,29 +16,57 @@ public class ProductoServiceImpl implements ProductoService{
 
 	@Autowired
 	private ProductoDao dao;
+	
+	@Autowired
+	private CategoriaDao categoriaDao;
+	
 	@Override
-	public Flux<Producto> findall() {
-		
+	public Flux<Producto> findAll() {
 		return dao.findAll();
 	}
 
 	@Override
 	public Mono<Producto> findById(String id) {
-		
 		return dao.findById(id);
 	}
 
 	@Override
 	public Mono<Producto> save(Producto producto) {
-		
 		return dao.save(producto);
 	}
 
 	@Override
-	public void delete(Producto producto) {
-		dao.delete(producto);
-		
-		
+	public Mono<Void> delete(Producto producto) {
+		return dao.delete(producto);
 	}
+
+	@Override
+	public Flux<Producto> findAllConNombreUpperCase() {
+		return dao.findAll().map(producto -> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		});
+	}
+
+	@Override
+	public Flux<Producto> findAllConNombreUpperCaseRepeat() {
+		return findAllConNombreUpperCase().repeat(5000);
+	}
+
+	@Override
+	public Flux<Categoria> findAllCategoria() {
+		return categoriaDao.findAll();
+	}
+
+	@Override
+	public Mono<Categoria> findCategoriaById(String id) {
+		return categoriaDao.findById(id);
+	}
+
+	@Override
+	public Mono<Categoria> saveCategoria(Categoria categoria) {
+		return categoriaDao.save(categoria);
+	}
+
 
 }
